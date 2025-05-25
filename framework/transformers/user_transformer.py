508da0bf37263c6e516cd470bc181b609ddd0a13
Stav <stav.h@openweb.com>
2025-05-25 09:13:59 +0300

    field names update

--- framework/transformers/user_transformer.py
+++ framework/transformers/user_transformer.py
@@ -9,9 +9,9 @@ class UserTransformer(DataTransformer):
             col("user_data.userPrincipalName").alias("user_principal_name"),
             when(col("user_data.mail").isNull(), col("user_data.otherMails")[0]).otherwise(col("user_data.mail")).alias("mail"),
             col("user_data.userType").alias("user_type"),
-            col("user_data.usageLocation").alias("usage_location"),
-            col("user_data.accountEnabled").alias("account_enabled"),
-            col("user_data.givenName").alias("given_name"),
-            col("user_data.surname").alias("surname")
+            col("user_data.usageLocation").alias("location"),
+            col("user_data.accountEnabled").alias("is_enabled"),
+            col("user_data.givenName").alias("first_name"),
+            col("user_data.surname").alias("last_name")
         )
         return user_data 
